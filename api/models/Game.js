ec23c301e0a71b50f76d85966d491d0524bc0e95
ildar <ildar.icoosoft@gmail.com>
2020-09-23 21:19:19 +0500

    add customToJson function to Game model

--- api/models/Game.js
+++ api/models/Game.js
@@ -5,6 +5,10 @@
  * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
  */
 
+const {
+  makeChessInstance
+} = sails.helpers;
+
 module.exports = {
 
   attributes: {
@@ -80,6 +84,35 @@ module.exports = {
     },
 
   },
+  customToJSON: function() {
+    const game = {...this};
+
+    if (game.status === "started") {
+      const chess = makeChessInstance(game);
+
+      if (chess.history().length > 2) {
+        let timePropName;
+        if (game.turn === "white") {
+          timePropName = "wtime";
+        } else {
+          timePropName = "btime";
+        }
+
+        game[timePropName] = game[timePropName] - (Date.now() - game.lastMoveAt);
+
+        if (game[timePropName] <=0) {
+          game[timePropName] = 0;
+        }
+
+        if (game[timePropName] === 0) {
+          game.status = "outoftime";
+          game.winner = game.turn === "white" ? "black" : "white";
+        }
+      }
+    }
+
+    return game;
+  }
 
 };
 
