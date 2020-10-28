9feef6b038bf303880e5750ad9224b768a9f8b52
ildar <ildar.icoosoft@gmail.com>
2020-10-29 01:43:30 +0500

    interm

--- config/session.js
+++ config/session.js
@@ -23,6 +23,7 @@ module.exports.session = {
   cookie: {
     secure: true,
     sameSite: "none",
+    maxAge: 24 * 60 * 60 * 1000 * 30,  // 30 days
   },
 
   /***************************************************************************
