d701c480fcd47cdd42aa3bded1a8d3b06bba6dec
ildar <ildar.icoosoft@gmail.com>
2020-10-29 13:11:43 +0500

    interm

--- config/env/production.js
+++ config/env/production.js
@@ -369,7 +369,7 @@ module.exports = {
   ssl: {
     key: fs.readFileSync('/etc/letsencrypt/live/zenproject.ru/privkey.pem'),
     cert: fs.readFileSync('/etc/letsencrypt/live/zenproject.ru/fullchain.pem')
-  }
+  },
 
 
 
