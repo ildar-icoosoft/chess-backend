a9a909029256b871310f5aeb41fa01b017a2fe0c
ildar <ildar.icoosoft@gmail.com>
2020-10-29 14:03:08 +0500

    interm

--- config/env/production.js
+++ config/env/production.js
@@ -379,7 +379,7 @@ module.exports = {
   *                                                                         *
   ***************************************************************************/
   custom: {
-    baseUrl: 'https://example.com',
+    baseUrl: 'https://zenproject.ru',
     internalEmailAddress: 'support@example.com',
 
     // sendgridSecret: 'SG.fake.3e0Bn0qSQVnwb1E4qNPz9JZP5vLZYqjh7sn8S93oSHU',
