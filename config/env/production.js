4f7e38fcb75be2408dd2f7c1a0e1b8ff4e7b3421
ildar <ildar.icoosoft@gmail.com>
2020-10-29 13:38:37 +0500

    interm

--- config/env/production.js
+++ config/env/production.js
@@ -223,7 +223,8 @@ module.exports = {
     *                                                                          *
     ***************************************************************************/
     cookie: {
-      // secure: true,
+      secure: true,
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,  // 24 hours
     },
 
